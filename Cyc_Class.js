25888164a77a518592b115513056465ef26f718c
cdewy0 <cdewy27@gmail.com>
2025-10-19 12:20:14 -0400

    added generation of random seed to ring of lights user must follow

--- Cyc_Class.js
+++ Cyc_Class.js
@@ -10,15 +10,15 @@ export class LED {
     this.lit = 0;
   }
 
-  on(target_index) {
+  on() {
     this.lit = 1;
   }
   
-  off(target_index) {
+  off() {
     this.lit = 0;
   }
   
-  toggle(target_index) {
+  toggle() {
     if(this.lit == 0) {
       this.lit = 1;
     } else {
@@ -55,7 +55,65 @@ export class LEDRing {
       this.angle_radians = this.angle_spacing * (Math.PI/180.0);
       this.ring_radius = (this.arc_separation + 2*this.LED_radius)/(this.angle_radians);
 
+      this.direction = "CW"; //alternative is "CCW"
+
+      this.seed_Array = [];
       this.LED_Array = []; 
+      
+      this.min_distance = 8;
+    
+      this.activeIndex = 0; //the index of the LED currently lit during the play of the game
+      this.seedIndex = 0;
+  }
+
+  generateSeed() {
+    let randomIndex = 0;
+    let chosenValue = 0;
+    let previousValue = 0;
+    let currentLength = 0;
+    let distance = 0;
+    let temp_array = [];
+    //fill with 0-47
+    for(var i = 0; i < this.number; i++) {
+      temp_array.push(i);
+    }
+    
+    while(temp_array.length > 0) {
+      randomIndex = Math.floor(temp_array.length*Math.random());
+      currentLength = this.seed_Array.length;
+      previousValue = this.seed_Array[currentLength - 1];
+      chosenValue = temp_array[randomIndex];
+      distance = Math.abs(chosenValue - previousValue);
+      if(distance < this.min_distance) { //select a new random value if distance is "unfair"
+        continue;
+      }
+
+      this.seed_Array.push(chosenValue);
+      temp_array.splice(randomIndex, 1); //praying
+    }
+    
+    console.log(this.seed_Array);  
+  }
+
+  nextInSeed() {
+    //at the start, initiate to the first position
+    if(this.seedIndex == 0) {
+      this.LED_Array[this.seedIndex].on();
+      this.seedIndex = this.seedIndex + 1;
+    }
+
+    this.LED_Array[this.activeIndex].off();
+    if(this.seedIndex == this.number) { //this condition actually implies the user has one, I believe
+      return;
+    } 
+    
+    let nextIndex = this.seed_Array[this.seedIndex];
+    this.LED_Array[nextIndex].on(); 
+    this.activeIndex = nextIndex;
+    this.seedIndex = this.seedIndex + 1;
+
+    //testing to see if it hits all the correct positions
+    //console.log(this.seedIndex); //gets up to 48
   }
 
   generateLEDs() {
@@ -97,7 +155,7 @@ export class LEDRing {
 
   blinkAll() {
     for(var i = 0; i < this.number; i++) {
-      this.LED_Array[i].toggle(i);
+      this.LED_Array[i].toggle();
     }
   }
   
--- CycloneMK1.js
+++ CycloneMK1.js
@@ -18,7 +18,13 @@ var BLINK_FREQ_DRAWS = BLINK_FREQ/60.0;
 
 var BLINK_PERIOD_DRAWS = parseInt(1/BLINK_FREQ_DRAWS);
 
-//LED_RATE = 2 //in LEDs/sec
+//note -- should be a property of an LED Ring that can be set from this file based upon difficulty
+//selector at the time of pushing the start button
+
+var LEDS_PER_SEC = 2 //in LEDs/sec
+var LEDS_PER_DRAW = LEDS_PER_SEC/60.0;
+var DRAWS_PER_MOVE = parseInt(1/LEDS_PER_DRAW);
+
 
 //drawing parameter constants
 
@@ -28,8 +34,11 @@ var LED_RAD = 10;
 //SEG_W
 //SEG_H
 
-//BUTTON_W
-//BUTTON_H
+var START_X = canvasWidth/2.0;
+var START_Y = canvasWidth*(3.0/4.0);
+var START_W = '120px';
+var START_H = '40px';
+var START_BCKD_COLOR = '#c25904';
 
 //arrays
 
@@ -44,6 +53,14 @@ var LED_RAD = 10;
 var USER_RING;
 var GAME_RING;
 
+//intended callback function for when spacebar is pressed
+window.keyPressed = function() {
+  if (key === ' ') {
+    console.log('Spacebar was pressed');
+    // This won't pause draw(), but if it takes long, it could cause lag
+  }
+}
+
 window.setup = function() {
   let RED_ON = color(255, 0, 0);
   let RED_OFF = color(150, 0, 0);
@@ -54,6 +71,29 @@ window.setup = function() {
   createCanvas(canvasWidth, canvasHeight);
 	background(150, 220, 255);
   
+  /*
+  let start_button = createButton('START');
+  
+  // Position the button
+  start_button.position(START_X, START_Y);
+  
+  // Set the width and height of the button
+  start_button.style('width', START_W);
+  start_button.style('height', START_H);
+  
+  // Set background color, border style, and font size
+  start_button.style('background-color', START_BCKD_COLOR); // Yellow background
+  start_button.style('border', '2px solid #000'); // Black border
+  start_button.style('font-size', '24px');
+  
+  // Set text color and padding
+  start_button.style('color', '#ffffff'); // White text
+  start_button.style('padding', '10px');
+  
+  // Set margin and cursor style
+  start_button.style('margin', '10px');
+  start_button.style('cursor', 'pointer');
+  */
   //constructor(color, number, LED_radius, arc_separation, centerX, centerY) {
   //console.log(LED_RAD);
   GAME_RING = new LEDRing(RED_ON, RED_OFF, LEDS_PER_RING, LED_RAD, 10, centerWidth, centerHeight);
@@ -61,6 +101,10 @@ window.setup = function() {
  
   GAME_RING.generateLEDs();
   USER_RING.generateLEDs();
+ 
+  //comment so this thing updates? 
+  GAME_RING.generateSeed();
+
 }
 
 window.draw = function() {
@@ -70,8 +114,9 @@ window.draw = function() {
   USER_RING.drawLEDs();
 
   if(drawCounts % parseInt(BLINK_PERIOD_DRAWS/2) == 0) {
-    console.log("toggle all");  
-    GAME_RING.blinkAll();
+    //console.log("toggle all");  
+    //GAME_RING.blinkAll();
+    GAME_RING.nextInSeed();
     USER_RING.blinkAll();
   }
     
